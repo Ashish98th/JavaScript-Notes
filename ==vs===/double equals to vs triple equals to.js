99dfb7e7cf0a4f99422674996fedf5fdc2e74802
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-10-27 12:14:12 +0530

    Add files via upload

---
+++ ==vs===/double equals to vs triple equals to.js
@@ -0,0 +1,10 @@
+let i=6;
+let j="6"
+
+console.log(i==j)
+console.log(i===j)
+
+
+
+// == used for comparing two values but ignores the data type 
+//===used for comparing two values,but this operator checks datatype and compare two values
