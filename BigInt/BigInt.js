55398c10192d41858d19abd374823349800554e6
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-11-24 14:13:35 +0530

    BigInt

---
+++ BigInt/BigInt.js
@@ -0,0 +1,9 @@
+let myNumber=BigInt(12432823213257456535365);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(myNumber)
+
+let myNumber2=BigInt(12)
+let sameMynumber=123n;
+console.log(myNumber2);
+console.log(sameMynumber);
