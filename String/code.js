e5c160393ee94420bc6f7f502c67b21db0d89d71
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-11-24 11:39:58 +0530

    Add files via upload

---
+++ String/code.js
@@ -0,0 +1,11 @@
+let name="Ashish";
+
+console.log(name[0]);
+
+//length of String
+
+//name.length
+
+console.log(name.length);
+
+console.log(name[name.length-1])
---
+++ String/string_methods.js
@@ -0,0 +1,8 @@
+let name="    Ashi    sh    ";
+
+console.log(name.trim());
+
+
+let name2="Shivshankar";
+
+console.log(name2.slice(0,4))
