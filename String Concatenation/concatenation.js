631446c3f1e2a190fc0f821369c8788cb63eecd8
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-11-24 11:39:12 +0530

    Add files via upload

---
+++ String Concatenation/concatenation.js
@@ -0,0 +1,22 @@
+//1
+
+let String1="Ashish";
+let String2="Nath";
+
+let String3=String1 + " " + String2;
+
+console.log(String3);
+
+
+//2
+let String4="45";
+let String5="55";
+
+console.log(String4+String5)
+
+//3
+let String6="45";
+let String7="56";
+
+console.log( +String6+ +String7)  //number
+
