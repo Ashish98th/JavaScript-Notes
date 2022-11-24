a89e89416e03fd73b03c931e8ed04e25eccc2799
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-11-24 11:52:46 +0530

    Add files via upload

---
+++ undefined/undefined.js
@@ -0,0 +1,13 @@
+//var 
+var firstname;
+console.log(firstname);
+
+//let
+let firstname4;
+console.log(firstname4);
+firstname4="Ashish";
+console.log(typeof firstname4,firstname4);
+
+//const
+// const firstname3;
+// console.log(firstname3);
